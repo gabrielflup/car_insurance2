751ea819cabfa8c5fb4e35323d082958ac6a63bf
gabrielflup <80179182+gabrielflup@users.noreply.github.com>
2024-11-25 13:59:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import os
 import requests
 import tensorflow as tf
 import uvicorn
+tf.config.set_visible_devices([], 'GPU')
 
 # Função para baixar o modelo do Google Drive
 def download_file_from_google_drive(file_id, destination):
